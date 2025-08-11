fa7744548e69a0052f582254ddc919f4becea222
Javohir Odilbekov <javohir_back@olcha.uz>
2025-08-11 19:12:53 +0500

    langs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Jobs\Interfaces\SubscriptionNotifier;
 use App\Jobs\services\TelegramSubscriptionNotifier;
+use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
+            $switch
+                ->visible(outsidePanels: true)
+                ->locales(['uz','ru', 'en']); // also accepts a closure
+        });
     }
 }
